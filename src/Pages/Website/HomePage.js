dd5b4618ec18912aa782f011a5173fdad5ab230c
KaramDargham <105083656+KaramDargham@users.noreply.github.com>
2025-07-03 11:21:21 +0300

    Update HomePage.js

--- src/Pages/Website/HomePage.js
+++ src/Pages/Website/HomePage.js
@@ -146,132 +146,86 @@ export default function HomePage() {
           </div>
         </section>
       )}
-      <section className="mt-8">
-        <div className="container mx-auto">
-          <div className="text-center border-b-0">
-            <h4 className="text-gray-700 text-3xl font-bold pt-10">
-              {t("popular-products")}
-            </h4>
-          </div>
-          <div className="mt-10 container flex justify-center">
-            <div className="w-[90%]">
-              <div className="grid grid-cols-12 gap-3">
-                {products.map((product, index) =>
-                  index !== products.length - 1 ? (
-                    <Link
-                      to={`/products/${product._id}`}
-                      className="bg-white shadow-md rounded-lg dark:bg-gray-800 dark:border-gray-700 col-span-12 md:col-span-4 flex justify-center items-center"
-                      key={index}
-                    >
-                      <div className="w-full">
-                        <div className="flex justify-center">
-                          <img
-                            className="rounded-t-lg p-8 w-64 h-52"
-                            src={
-                              product.imagesUrl !== null
-                                ? `https://store-3t4b.onrender.com${product.imagesUrl[0]}`
-                                : ""
-                            }
-                            alt={product.name}
-                          />
-                        </div>
-                        <div className="px-5 pb-5">
-                          <h3 className="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">
-                            {product.name}
-                          </h3>
-                          <div className="flex items-center mt-2.5 mb-5">
-                            {[...Array(5)].map((_, i) => (
-                              <svg
-                                key={i}
-                                className="w-5 h-5 text-yellow-300"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                              </svg>
-                            ))}
-                            <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                              5.0
-                            </span>
-                          </div>
-                          <div className="flex items-center justify-between">
-                            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                              ${product.price}
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </Link>
-                  ) : (
-                    <div className="w-full md:flex col-span-12 md:col-span-8 gap-3">
-                      <Link
-                        to={`/products/${product._id}`}
-                        className="bg-white shadow-md rounded-lg dark:bg-gray-800 dark:border-gray-700 w-full  flex justify-center items-center"
-                        key={index}
+     <section className="mt-8">
+  <div className="container mx-auto">
+    <div className="text-center border-b-0">
+      <h4 className="text-gray-700 text-3xl font-bold pt-10">
+        {t("popular-products")}
+      </h4>
+    </div>
+    <div className="mt-10 container flex justify-center">
+      <div className="w-[90%]">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {/* First render all products in 3-column grid */}
+          {products.map((product, index) => (
+            <Link
+              to={`/products/${product._id}`}
+              className="bg-white shadow-md rounded-lg dark:bg-gray-800 dark:border-gray-700 flex justify-center items-center"
+              key={index}
+            >
+              <div className="w-full">
+                <div className="flex justify-center">
+                  <img
+                    className="rounded-t-lg p-8 w-64 h-52"
+                    src={
+                      product.imagesUrl !== null
+                        ? `https://store-3t4b.onrender.com${product.imagesUrl[0]}`
+                        : ""
+                    }
+                    alt={product.name}
+                  />
+                </div>
+                <div className="px-5 pb-5">
+                  <h3 className="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">
+                    {product.name}
+                  </h3>
+                  <div className="flex items-center mt-2.5 mb-5">
+                    {[...Array(5)].map((_, i) => (
+                      <svg
+                        key={i}
+                        className="w-5 h-5 text-yellow-300"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
                       >
-                        <div className="w-full">
-                          <div className="flex justify-center">
-                            <img
-                              className="rounded-t-lg p-8 w-64 h-52"
-                              src={
-                                product.imagesUrl !== null
-                                  ? `https://store-3t4b.onrender.com${product.imagesUrl[0]}`
-                                  : ""
-                              }
-                              alt={product.name}
-                            />
-                          </div>
-                          <div className="px-5 pb-5">
-                            <h3 className="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">
-                              {product.name}
-                            </h3>
-                            <div className="flex items-center mt-2.5 mb-5">
-                              {[...Array(5)].map((_, i) => (
-                                <svg
-                                  key={i}
-                                  className="w-5 h-5 text-yellow-300"
-                                  fill="currentColor"
-                                  viewBox="0 0 20 20"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                >
-                                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                                </svg>
-                              ))}
-                              <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                                5.0
-                              </span>
-                            </div>
-                            <div className="flex items-center justify-between">
-                              <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                                ${product.price}
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                      </Link>
-                      <Link
-                        to={`/shop`}
-                        className="bg-white shadow-md rounded-lg w-full md:mt-0 mt-3 pb-3 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-evenly items-center"
-                        key={index}
-                      >
-                        <div className="w-full">
-                          <div className="flex justify-center">
-                            <span className="mdi mdi-storefront-plus text-[100px] text-primaryColor transition duration-200 hover:text-neutral-500"></span>
-                          </div>
-                        </div>
-                        <div className="flex justify-center pt-5">
-                          <h5 className="text-xl">{t("shop-more")}</h5>
-                        </div>
-                      </Link>
-                    </div>
-                  )
-                )}
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                      </svg>
+                    ))}
+                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+                      5.0
+                    </span>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                      ${product.price}
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
+            </Link>
+          ))}
+          
+          {/* Then add the "Shop More" card in a new row if needed */}
+          <div className="col-span-1 md:col-span-3">
+            <Link
+              to={`/shop`}
+              className="bg-white shadow-md rounded-lg w-full py-8 dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-evenly items-center"
+            >
+              <div className="w-full">
+                <div className="flex justify-center">
+                  <span className="mdi mdi-storefront-plus text-[100px] text-primaryColor transition duration-200 hover:text-neutral-500"></span>
+                </div>
+              </div>
+              <div className="flex justify-center pt-5">
+                <h5 className="text-xl">{t("shop-more")}</h5>
+              </div>
+            </Link>
           </div>
         </div>
-      </section>
+      </div>
+    </div>
+  </div>
+</section>
       <div className="pt-24"></div>
     </div>
   );
